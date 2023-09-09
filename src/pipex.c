3495e39c93ff19d91b3358bdf05c7f8b712fc384
David Garcia Varas <davidga2@c1r15s4.42madrid.com>
2023-09-09 15:23:09 +0200

    Arreglo de hijos con psegura-

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 23:37:02 by davidga2          #+#    #+#             */
-/*   Updated: 2023/09/08 05:52:26 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/09/09 15:20:26 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char **ft_get_paths(char **envp)
 	char **matrix;
 
 	i = 0;
-	mtx = NULL;
+	matrix = NULL;
 	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
 		i++;
 	if (!envp[i]) // Esto evita un segmantation fault en caso de no existir la variable PATH.
@@ -49,7 +49,7 @@ char **ft_get_paths(char **envp)
 // Esto es un caos. Spliteo PATH, los argumentos del comando del infile y,
 // en principio, tras ello tocaría ejecutar el comando. Pero ni idea de como
 // gestionar los errores.
-void	ft_exec(char binaries, char **envp)
+void	ft_exec(char **argv, char **envp)
 {
 	char	**binaries;
 	char	**cmd;
@@ -70,41 +70,77 @@ void	ft_exec(char binaries, char **envp)
 	}
 }
 
-// El infile_child creo que está bien.
+// El hijo del final ha de cerrar el pipe entero. Por lo que debe hacer 3 "close()".
+// Output.
+void	ft_outfile_child(char **argv, char **envp, int *pipe_fd)
+{
+	int		outfile_fd;
+	pid_t	child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+		ft_error("Outfile child pid creation proccess failed");
+	if (child_pid == 0)
+	{
+		if (close(pipe_fd[1]) == -1)
+			ft_error("Some fd close failed in the outfile child proccess-.-.-");
+		outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		if (outfile_fd == -1 || dup2(pipe_fd[0], STDIN_FILENO) == -1
+			|| dup2(outfile_fd, STDOUT_FILENO) == -1)
+			ft_error("Outfile fd creation or dup2 failed");
+		if (close(pipe_fd[0]) == -1 || close(outfile_fd) == -1)
+			ft_error("Some fd close failed in the output child proccess");
+		if (ft_exec(argv, envp) == -1)
+			ft_error("Nothing executed");
+	}
+
+}
+
+// El infile_child creo que está bien. Input.
 void	ft_infile_child(char **argv, char **envp, int *pipe_fd)
 {
 	int		infile_fd;
 	pid_t	child_pid;
 
-	infile_fd = open(argv[1], O_RDONLY);
 	child_pid = fork();
-	if (child_pid < 0 || infile_fd < 0)
-		ft_error("Infile fd open or infile child creation proccess failed");
+	if (child_pid == -1)
+		ft_error("Infile child pid creation proccess failed");
 	if (child_pid == 0)
 	{
-		if (dup2(infile_fd, STDIN_FILENO) < 0
-			|| dup2(pipe_fd[0], STDOUT_FILENO < 0)
-			ft_error("dup2 failed on its execution changing fds");
-		ft_exec(argv, envp);
+		if (close(pipe_fd[0]) == -1)
+			ft_error("Some fd close failed in the infile child proccess-.-.-");
+		infile_fd = open(argv[1], O_RDONLY);
+		if (infile_fd == -1 || dup2(infile_fd, STDIN_FILENO) == -1
+			|| dup2(pipe_fd[1], STDOUT_FILENO == -1))
+			ft_error("Infile fd creation or dup2 failed");
+		if (close(infile_fd) == -1 || close(pipe_int[1]) == -1)
+			ft_error("Some fd close failed in the infile child proccess");
+		if (ft_exec(argv, envp) == -1)
+			ft_error("Nothing executed");
 	}
+	// el padre tiene que esperar con waitpid a este hijo?
+	// close (pipe_fd[1]);
 }
 
 // En construcción. Equivale al padre.
 void ft_pipex(char **argv, char **envp)
 {
-	//int		outfile_fd;
 	int		pipe_fd[2];
 
 	if (pipe(pipe_fd) == -1)
 		ft_error("La vinculación de fd en la creación del pipe ha fallado");
-	//outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	ft_infile_child(argv, envp, pipe_fd);
+	ft_outfile_child(argv, envp, pipe_fd);
+	if (close(pipe_fd[0]) == -1 || close(pipe_fd[1] == -1))
+		ft_error("Parent cant close some pipe_fd");
+	waitpid(-1, NULL, 0);
+	waitpid(-1, NULL, 0);
 }
 
 // En construcción.
 int	main(int argc, char **argv, char **envp)
 {
-	if (argc == 3)
+	if (argc == 5)
 		ft_pipex(argv, envp);
 	else
 		return (1);
