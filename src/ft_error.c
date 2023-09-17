451dedbf42cead722277afd14b4ea114b96d20fc
David Garcia Varas <davidga2@c1r5s6.42madrid.com>
2023-09-17 23:19:49 +0200

    Función para el error de comando no encontrado añadida.

--- src/ft_error.c
+++ src/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 20:21:44 by davidga2          #+#    #+#             */
-/*   Updated: 2023/09/14 20:23:28 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/09/17 23:16:59 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 // Función para reventar la ejecución. vicmarti dice que no está de acuerdo
 // al retornar el "errno". Con algo que no sea 0 le vale.
-void	ft_error(char *msg)
+void	ft_error(char *str)
 {
-	perror(msg);
+	perror(str);
 	exit(errno);
+}
+
+// Función dedicada a los errores de comandos no encontrados.
+void	ft_cmd_error(char *cmd)
+{
+	ft_printf_error("%s", cmd);
+	ft_printf_error(": command not found\n");
+	exit(127);
 }
